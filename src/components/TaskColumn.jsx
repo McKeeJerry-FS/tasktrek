52c73411ea05f3a6d3e3042b629b143711ded9d4
Jerry McKee Jr <jmckee0882@student.fullsail.edu>
2025-03-05 20:05:15 -0500

    created Task Cards

--- src/components/TaskColumn.jsx
+++ src/components/TaskColumn.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import TaskCard from './TaskCard'
 
 
 import './TaskColumn.css'
@@ -7,6 +8,7 @@ const TaskColumn = ({taskHeading, taskIcon}) => {
   return (
     <section className="task_column">
       <h2 className='task_column_heading'><img src={taskIcon} className='task_column_icon' alt="" />{taskHeading}</h2>
+      <TaskCard />
     </section>
   );
 }
