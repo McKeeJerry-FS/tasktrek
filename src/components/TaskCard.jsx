a062e696c158bf4b261fbce6090ca905de97a975
McKeeJerry-FS <jerry.mckee.jr0882@gmail.com>
2025-03-08 10:47:30 -0500

    added a task card componenet

---
+++ src/components/TaskCard.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import './TaskCard.css'
+
+const TaskCard = () => {
+  return (
+    <article className="task_card">
+      
+    </article>
+
+
+
+  )
+}
+
+export default TaskCard
